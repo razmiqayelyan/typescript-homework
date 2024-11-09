22b04a0f0468557e9130448db40a80cb3372dc1f
razmiqayelyan <raz.miqayelyan@gmail.com>
2024-11-08 18:23:48 -0800

    3rd Homework

--- index.ts
+++ index.ts
@@ -1,62 +1,36 @@
-interface PostType {
-    id: string;
-    title: string;
-    body: string;
+
+
+type CommentType = {
+    id: number
+    email: string
 }
 
-const posts: PostType[] = [
-    {
-        id: '62e69d5a5458aac0ed320b35',
-        title: 'id labore ex et quam laborum',
-        body: 'laudantium enim quasi est quidem magnam voluptate ipsam eostempora quo necessitatibusdolor quam autem quasireiciendis et nam sapiente accusantium'
-    },
-    {
-        id: '62e69d5a5458aac0ed320b1c',
-        title: 'quo vero reiciendis velit similique earum',
-        body: 'est natus enim nihil est dolore omnis voluptatem numquamet omnis occaecati quod ullam at voluptatem error expedita pariaturnihil sint nostrum voluptatem reiciendis et'
-    },
-    {
-        id: '62e69d5a5458aac0ed320b32',
-        title: 'odio adipisci rerum aut animi',
-        body: 'quia molestiae reprehenderit quasi aspernaturaut expedita occaecati aliquam eveniet laudantiumomnis quibusdam delectus saepe quia accusamus maiores nam estcum et ducimus et vero voluptates excepturi deleniti ratione'
-    },
-    {
-        id: '62e69d5a5458aac0ed320b39',
-        title: 'alias odio sit',
-        body: 'non et atqueoccaecati deserunt quas accusantium unde odit nobis qui voluptatemquia voluptas consequuntur itaque doloret qui rerum deleniti ut occaecati'
-    },
-    {
-        id: '62e69d5a5458aac0ed320b53',
-        title: 'vero eaque aliquid doloribus et culpa',
-        body: 'harum non quasi et rationetempore iure ex voluptates in rationeharum architecto fugit inventore cupiditatevoluptates magni quo et'
-    },
-    {
-        id: '62e69d5a5458aac0ed320b19',
-        title: 'et fugit eligendi deleniti quidem qui sint nihil autem',
-        body: 'doloribus at sed quis culpa deserunt consectetur qui praesentiumaccusamus fugiat dictavoluptatem rerum ut voluptate autemvoluptatem repellendus aspernatur dolorem in'
-    },
-    {
-        id: '62e69d5a5458aac0ed320b25',
-        title: 'repellat consequatur praesentium vel minus molestias voluptatum',
-        body: 'maiores sed dolores similique labore et inventore etquasi temporibus esse sunt id eteos voluptatem aliquamratione corporis molestiae mollitia quia et magnam dolor'
+
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
+
+// Define the getData function
+const getData = async (url: string) : Promise<CommentType[] | undefined> => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from ${url}`);
     }
-];
 
-interface OrganizedPostsType {
-    byId: { [id: string]: PostType };
-    allIds: string[];
-}
-
-const normalizeData = (unnormalizedData: PostType[]): OrganizedPostsType => {
-    return unnormalizedData.reduce(
-        (acc, post) => {
-            acc.byId[post.id] = post;
-            acc.allIds.push(post.id);
-            return acc;
-        },
-        { byId: {}, allIds: [] } as OrganizedPostsType
-    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 };
 
-const normalizedPosts: OrganizedPostsType = normalizeData(posts);
-console.log(normalizedPosts);
+
+// Call the getData function and log the data
+getData(COMMENTS_URL)
+  .then(data => {
+    if (data) {
+      data.forEach((comment:CommentType):void => {
+        console.log(`ID: ${comment.id}, Email: ${comment.email}`);
+      });
+    }
+  });
